0c75f0cbed920c2fc06223d02d66e69707409880
ThadDuval <ThadDuval.lavuD@gmail.com>
2015-06-09 14:26:12 -0400

    AddedFacebook

---
+++ config/auth.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : '883725041665571', // your App ID
+        'clientSecret'  : '24f4f25628602bb5c968a5a48f01189f', // your App Secret
+        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+    },
+
+    'twitterAuth' : {
+        'consumerKey'       : 'your-consumer-key-here',
+        'consumerSecret'    : 'your-client-secret-here',
+        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+    },
+
+    'googleAuth' : {
+        'clientID'      : 'your-secret-clientID-here',
+        'clientSecret'  : 'your-client-secret-here',
+        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+    }
+
+};
